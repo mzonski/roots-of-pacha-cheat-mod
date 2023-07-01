31900ada010ea8e19628fdef7fe3d2fe287cbf3a
Maciej Zonski <me@zonni.pl>
2023-07-01 18:02:40 +0200

    (feature): even easier fishing

--- CheatMod.Core/Patches/EasyFishing.cs
+++ CheatMod.Core/Patches/EasyFishing.cs
@@ -7,10 +7,43 @@ public partial class CheatModPatches
 {
     [HarmonyPatch(typeof(FishingHitMode), "FocusRise", MethodType.Getter)]
     [HarmonyPrefix]
-    private static bool EasyFishingPatch(ref float __result)
+    private static bool FishingFocusRisePatch(ref float __result)
     {
         if (!CheatOptions.IsEasyFishingEnabled) return true;
         __result = 1000f;
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(FishingMinigame), "FishSpawnBuff", MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool FishingSpawnBuffPatch(ref float __result)
+    {
+        if (!CheatOptions.IsEasyFishingEnabled) return true;
+        __result = 1000f;
+        return false;
+    }
+    [HarmonyPatch(typeof(FishingMinigame), "FishCatchBuff", MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool FishingCatchBuffPatch(ref float __result)
+    {
+        if (!CheatOptions.IsEasyFishingEnabled) return true;
+        __result = 1000f;
+        return false;
+    }
+    [HarmonyPatch(typeof(FishingMinigame), "FishRarityBuff", MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool FishingRarityBuffPatch(ref float __result)
+    {
+        if (!CheatOptions.IsEasyFishingEnabled) return true;
+        __result = 1000f;
+        return false;
+    }
+    [HarmonyPatch(typeof(FishingShoalController), "SpawnRate", MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool FishingRarityBuffPatch(ref int __result)
+    {
+        if (!CheatOptions.IsEasyFishingEnabled) return true;
+        __result = 1;
         return false;
     }
 }
